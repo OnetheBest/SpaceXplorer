2b4864c628d17054cb2d37d024170b29c3258094
Nathan <nathoni2019@gmail.com>
2025-04-14 12:01:49 +0100

    first version

---
+++ spaceXplorer/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int difficulty;
+    printf("============================================\n");
+    printf("================SPACE XPLORER===============\n");
+    printf("Select Difficulty:\n1 - Easy\n2-Normal\n3-Hard\n4-Nightmare\n");
+    scanf("%c", &difficulty);
+
+    if (difficulty = 1) {
+        printf("easy");
+    }
+    return 0;
+}
