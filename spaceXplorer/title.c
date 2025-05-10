72e1d40383eb8bc48e1e7cc866d2513df2c636b3
Nathan <nathoni2019@gmail.com>
2025-05-10 02:17:40 +0100

    Added on-request inventory management (on press of I). Tutorial screen added

--- spaceXplorer/title.c
+++ spaceXplorer/title.c
@@ -4,21 +4,43 @@
 #include <windows.h>
 #include "scores.h"
 
+void showTutorial() {
+    system("cls");
+    printf("=== TUTORIAL ===\n");
+    printf("- Use W A S D to move.\n");
+    printf("- Press E to fire a bullet (if you have one).\n");
+    printf("- Collect ! for bullet and + for fuel.\n");
+    printf("- Avoid enemies (V), or shoot them for +100 points.\n");
+    printf("- Press I to open your inventory(player stats).\n");
+    printf("- Score increases over time.\n");
+    printf("- Game ends if you run out of fuel or health.\n");
+    printf("\nPress any key to return to title screen...");
+    _getch();
+}
+
 Difficulty titleScreen() {
     int highScore, lastScore;
     loadScores(&highScore, &lastScore);
     int diff;
-        printf("==================================");
-        printf("           SPACE XPLORER          ");
-        printf("==================================\n");
-        printf("\nHigh Score : %d\n", highScore);
-        printf("Last Score : %d\n", lastScore);
-        printf("\nChoose your difficulty:\n");
-        printf("1. Easy\n");
-        printf("2. Normal\n");
-        printf("3. Hard\n");
-        printf("4. Nightmare\n");
-        diff = _getch() - '0';
-    fflush(stdin);
-    return (Difficulty) diff;
-}
+
+    while(1){
+        system("cls");
+    printf("==================================");
+    printf("           SPACE XPLORER          ");
+    printf("==================================\n");
+    printf("\nHigh Score : %d\n", highScore);
+    printf("Last Score : %d\n", lastScore);
+    printf("\nChoose your difficulty:\n");
+    printf("1. Easy\n");
+    printf("2. Normal\n");
+    printf("3. Hard\n");
+    printf("4. Nightmare\n");
+    printf("5. Tutorial");
+    diff = _getch() - '0';
+
+    if (diff >= 1 && diff <= 4) {
+        return (Difficulty) diff;
+    } else if (diff == 5) {
+        showTutorial();
+    }
+}}
